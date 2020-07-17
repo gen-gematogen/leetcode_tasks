d3b91fe2a9f06eb1f87e9492435840c21beff3b3
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-17 11:04:13 +0300

    Create 141.cpp

---
+++ 141.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr){
+            return false;
+        }
+
+        ListNode* next;
+        ListNode* cur = head;
+
+        while (cur->next != nullptr && cur->next->next != head){
+            next = cur->next;
+            cur->next = head;
+            cur = next;
+        }
+
+        if (cur->next == nullptr){
+            return false;
+        }
+
+        return true;
+    }
+};
