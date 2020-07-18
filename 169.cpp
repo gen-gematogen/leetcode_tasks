20e0bd2afe525a761ec85399ab917da6fd708184
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-18 19:23:49 +0300

    REPEAT!

---
+++ 169.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = nums[0];
+        int cnt = 1;
+
+        for (int i = 1; i < nums.size(); ++i){
+            if (cnt == 0){
+                major = nums[i];
+            }
+            
+            if (nums[i] == major){
+                cnt++;
+            } else{
+                cnt--;
+            }
+        }
+
+        return major;
+    }
+};
