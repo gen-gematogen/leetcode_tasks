4899673666c6124b398e80cacaa8bf5b175d6859
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-21 23:17:06 +0300

    Create 876.cpp

---
+++ 876.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* mid = head;
+        ListNode* cur = head;
+        
+        while (cur->next){
+            cur = cur->next;
+            mid = mid->next;
+            
+            if (cur->next) cur = cur->next;
+        }
+        
+        return mid;
+    }
+};
