4d8338a1cc85ba1a96682d254d4acc97ab5f68d6
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-21 16:14:20 +0300

    Create 283.cpp

---
+++ 283.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int pos = 0;
+        
+        for (int i = 0; i < n; ++i){
+            if (nums[i]){
+                nums[pos++] = nums[i];
+            }
+        }
+        
+        for (int i = pos; i < n; ++i){
+            nums[i] = 0;
+        }
+    }
+};
