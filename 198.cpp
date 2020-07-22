b697e433b45b1afd7f4275b5c86c028ae22920f7
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-22 21:54:31 +0300

    Update 198.cpp

--- 198.cpp
+++ 198.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    /*int rob(vector<int>& nums) { //with rray
         int n = nums.size();
         
         if (n == 0) return 0;
@@ -16,7 +16,25 @@ public:
             money[i] = max(0, maxMoney) + nums[i];
             maxMoney = max(maxMoney, money[i - 1]);
         }
-        
         return max(maxMoney, money[n - 1]);
+    }*/
+    
+    int rob(vector<int>& nums){ //without array
+        int n = nums.size();
+        
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+    
+        int  prevMax = nums[0];
+        int last = nums[1];
+        int cur;
+        
+        for (int i = 2; i < n; ++i){
+            cur = max(prevMax, 0) + nums[i];
+            prevMax = max(prevMax, last);
+            last = cur;
+        }
+        
+        return max(prevMax, last);
     }
 };
