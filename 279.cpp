6abf33cbbfbad5e946b616e9b655243c92968488
gen-gematogen <47857394+gen-gematogen@users.noreply.github.com>
2020-07-25 23:52:13 +0300

    Create 279.cpp

---
+++ 279.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSquares(int n) { //DP
+        vector<int> nums(n + 1, INT_MAX);
+        
+        nums[0] = 0;
+        
+        for (int i = 1; i <= n; ++i){
+            for (int j = 1; i - j * j >= 0; ++j){
+                nums[i] = min(nums[i], nums[i - j * j] + 1);
+            }
+        }
+        
+        return nums[n];
+    }
+};
